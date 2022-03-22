b92ba134d2e9e856b9ead91098e1d3474380e77d
basilshah <42776530+basilshah@users.noreply.github.com>
2022-03-22 12:22:02 +0530

    Update servi.py

--- servi.py
+++ servi.py
@@ -12,3 +12,15 @@ sleep(0.5)
 servo.max()
 sleep(0.5)
 
+def SetAngle(angle):
+    duty = angle / 18 + 2
+	  GPIO.output(03, True)
+	  pwm.ChangeDutyCycle(duty)
+	  sleep(1)
+	  GPIO.output(03, False)
+	  pwm.ChangeDutyCycle(0)
+while True:
+    x = int(input("enter the angle"))
+    SetAngle(x)
+    
+
